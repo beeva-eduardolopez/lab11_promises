bbf75302d2750064c72b901fe4a88b69a315e075
beeva-eduardolopez <eduardo.lopez.anta@bbva.com>
2016-03-10 12:58:35 -0600

    format

--- Tutorial/rejectOrNo.js
+++ Tutorial/rejectOrNo.js
@@ -19,13 +19,13 @@
 //    console.log(reason.value);
 // });
 
- var promise = new Promise(function (fulfill, reject) {
-      fulfill('I FIRED');
-      reject(new Error('I DID NOT FIRE'));
-    });
-    
-    function onReject(error) {
-      console.log(error.message);
-    }
-    
-    promise.then(console.log, onReject);
+var promise = new Promise(function (fulfill, reject) {
+    fulfill('I FIRED');
+    reject(new Error('I DID NOT FIRE'));
+});
+
+function onReject(error) {
+    console.log(error.message);
+}
+
+promise.then(console.log, onReject);
